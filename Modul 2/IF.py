fd0fac9ae4913ce9abcb0a1c4c2ccb089212e441
Ananda Aulia <72482619+Ananda28@users.noreply.github.com>
2021-04-17 21:34:28 +0700

    Add files via upload

---
+++ Modul 2/IF.py
@@ -0,0 +1,20 @@
+x = False
+y = True
+
+#if..else
+if x != True :
+    print('print')
+else:
+    print('tidak print')
+
+#if..else if..else
+if x != False :
+    print('print c')
+elif y != False :
+    print('print y')
+
+#if(..and..)..else
+if x != True and y == False :
+    print('print')
+else:
+    print('tidak print')
---
+++ Modul 2/fungsiLamda.py
@@ -0,0 +1,7 @@
+#fungsi lambda
+
+maks = lambda  a,b : a if a > b else b
+print(maks(25, 20))
+
+same = lambda a : True if a == 25 else False
+print(same(20))
